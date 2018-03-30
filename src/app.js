9ce63371776e80a051d3f09707c17b9c6d70d1da
lucerohospina <lucerojhp@gmail.com>
2018-03-30 11:17:24 -0500

    setting uo webpack

--- src/app.js
+++ src/app.js
@@ -1,152 +1,16 @@
-class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.state = {
-      options: props.options
-    }
-  }
-  handleDeleteOptions() {
-    this.setState(() => ({options: []}));
-  }
-  handleDeleteOption(optionToRemove) {
-    this.setState((prevState) => ({
-      options:prevState.options.filter((option) => optionToRemove !== option)
-    }));
-  }
-  handlePick() {
-    const randomNumber = Math.floor(Math.random() * this.state.options.length);
-    const option = this.state.options[randomNumber];
-    alert(option);
-  }
-  handleAddOption(option) {
-    if(!option) {
-      return "Enter valid value to add item";
-    } else if (this.state.options.indexOf(option) > -1) {
-      return "This option already exists";
-    }
+// import './utils';
+// import substract, {square, add} from './utils.js';
+// import { isAdult, canDrink } from './person';
+// import isSenior from './person';
+import validator from 'validator';
 
-    this.setState((prevState) => ({options: prevState.options.concat([option])}));
-  }
-  render() {
-    const subtitle = "Put your life in the hands of a computer.";
+// console.log('app.js  is running!');
+// console.log(square(4));
+// console.log(add(100, 23));
+// console.log(substract(100, 81));
+// console.log(isSenior(65));
 
-    return (
-      <div>
-        <Header subtitle={subtitle}/>
-        <Action 
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options 
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
-      </div>
-    );
-  }
-}
+// console.log(isAdult(17));
+// console.log(canDrink(21));
 
-IndecisionApp.defaultProps = {
-  options: []
-}
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      {props.subtitle && <h2>{props.subtitle}</h2>}
-    </div>
-  );
-}
-
-Header.defaultProps = {
-  title: "Indecision"
-};
-
-const Action = (props) => {
-  return (
-    <div>
-      <button 
-        onClick={props.handlePick}
-        disabled={!props.hasOptions}>
-        What should I do?
-      </button>
-    </div>
-  );
-};
-
-const Options = (props) => {
-  return (
-    <div>
-    <button onClick={props.handleDeleteOptions}>Remove All</button>
-      <ul>
-        {
-          props.options.map((option) => 
-            <Option 
-              key={option} 
-              optionText={option}
-              handleDeleteOption={props.handleDeleteOption}
-            />)
-        }
-      </ul>
-    </div>
-  );
-}
-
-const Option = (props) => {
-  return (
-    <li>
-      {props.optionText}
-      <button 
-        onClick={(e) => {
-          props.handleDeleteOption(props.optionText);
-        }}
-      >
-        remove
-      </button>
-    </li>
-  );
-};
-
-class AddOption extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      error: undefined
-    };
-  }
-  handleAddOption(e) {
-    e.preventDefault();
-    console.log(e.target.elements.option.value);
-
-    const option = e.target.elements.option.value.trim();
-    const error = this.props.handleAddOption(option);
-
-    this.setState(() => ({error}));
-  }
-  render() {
-    return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.handleAddOption}>
-          <input type="text" name="option"/>
-          <button>Add Option</button>
-        </form>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <IndecisionApp/>,
-  document.getElementById('app')
-);
+console.log(validator.isEmail("test@gmail.com"));
