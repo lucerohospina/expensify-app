26dbbaa34b8c9df9ec15cd3c147519ba5f37389d
lucerohospina <lucerojhp@gmail.com>
2018-03-30 11:36:18 -0500

    setting up babel to work with webpack

--- src/app.js
+++ src/app.js
@@ -1,16 +1,9 @@
-// import './utils';
-// import substract, {square, add} from './utils.js';
-// import { isAdult, canDrink } from './person';
-// import isSenior from './person';
-import validator from 'validator';
+import React from 'react';
+import ReactDom from 'react-dom';
 
-// console.log('app.js  is running!');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(substract(100, 81));
-// console.log(isSenior(65));
+const template = <p>This is JSX from webpack!</p>;
 
-// console.log(isAdult(17));
-// console.log(canDrink(21));
-
-console.log(validator.isEmail("test@gmail.com"));
+ReactDom.render(
+  template, 
+  document.getElementById('app')
+);
